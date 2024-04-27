6a4dc8695e9a3c681016999086a5ea5fa3e315d0
abdulrehmarnAli <abdulrehmarnali@gmail.com>
2024-04-27 21:33:08 +0100

    Able to communicate with border cells and game area cells individually.

--- snake_logic.js
+++ snake_logic.js
@@ -1,6 +1,8 @@
 const gridContainer = document.getElementById('gameContainer');
 const gameGridSize = 24;
 const fullGridSize = gameGridSize + 2;
+let borderDivClass = 'borderCell';
+let gameGridDivClass = 'gameCell'
 
 function populateGrid() {
     for(let i = 0; i < fullGridSize*fullGridSize; i++) {
@@ -11,8 +13,6 @@ function populateGrid() {
 
 function populateGrid2() {
     // have set a variable to cell type class names so they can easily be changed in the future
-    let borderDivClass = 'borderCell';
-    let gameGridDivClass = 'gameCell'
     // whilst stepping through each row, also step through each column
     // this ensures all cells have been stepped through
     for(let row=0; row < fullGridSize; row++) {
@@ -21,18 +21,31 @@ function populateGrid2() {
             let cell = document.createElement('div')
             if(row === 0 | row === (fullGridSize-1) | column === 0 | column === (fullGridSize-1)){
                 cell.classList.add(borderDivClass)
-                cell.id = 'pix'+row.toString()+column.toString();
+                cell.id = 'x'+row.toString()+'y'+column.toString();
                 gridContainer.appendChild(cell)
             }
             else {
                 cell.className = gameGridDivClass
+                cell.classList.add(gameGridDivClass)
+                cell.id = 'x'+row.toString()+'y'+column.toString();
+                gridContainer.appendChild(cell)
             }
         }
     }
 }
 
-function GridIntroAnimation() {
 
+// after starting the below function, thinking this could be more easily achived (maybe) bu utilising grid-template-areas
+function GridIntroAnimation() {
+    let childDivs = gridContainer.getElementsByTagName('div');
+    // console.log(childDivs)
+    for(i=0; i < childDivs.length; i++) {
+        if(childDivs[i].classList.contains(borderDivClass)){
+            console.log(childDivs[i])
+            childDivs[i].style += '; background-color: red';
+        }
+    }
 }
 
 populateGrid2()
+GridIntroAnimation()
